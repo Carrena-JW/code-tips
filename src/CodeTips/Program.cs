7205ca01a28edb92b72d5ff7567037c0188bbc94
Ji-Woong <carrena1642@gmail.com>
2024-09-09 15:31:26 +0900

    added: parse datetime

--- src/CodeTips/Program.cs
+++ src/CodeTips/Program.cs
@@ -2,10 +2,14 @@
 
 
 using CodeTips.Linq;
+using CodeTips.Parse;
 
-var zipExample = new Zip();
-zipExample.Exmaple1();
+//var zipExample = new Zip();
+//zipExample.Exmaple1();
 
+
+var parseDatetime = new ParseDatetime();
+parseDatetime.Example1();
 
 
 
