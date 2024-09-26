038adcbfd9e5bd23ee271bb3aa8be17d8b5fc5dd
Ji-Woong <carrena1642@gmail.com>
2024-09-26 16:02:46 +0900

    commit:

---
+++ src/CodeTips/Configuration/AppSettingInConsole.cs
@@ -0,0 +1,52 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace CodeTips.Configuration
+{
+    internal class AppSettingInConsole
+    {
+        internal void Init()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            // 서비스 제공자를 빌드합니다.
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // AppSettings 인스턴스를 가져옵니다.
+            var appSettings = serviceProvider.GetRequiredService<IOptions<SettingValues>>().Value;
+        }
+
+        private  void ConfigureServices(IServiceCollection services)
+        {
+            // IConfigurationBuilder를 사용하여 구성 설정을 빌드합니다.
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+
+
+            services.Configure<SettingValues>((x) =>
+            {
+                configuration.GetSection("asd");
+            });
+               
+        }
+
+        internal void Example1()
+        {
+
+        }
+    }
+
+
+    public class SettingValues
+    {
+        public string ExportPath { get; set; } = string.Empty;
+        public string ImportPath { get; set; } = string.Empty;
+    }
+}
