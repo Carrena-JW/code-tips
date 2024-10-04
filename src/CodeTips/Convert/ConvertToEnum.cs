04f3aec4b8484ca1144544c2381b3a3d44b859b6
Ji-Woong <carrena1642@gmail.com>
2024-10-04 15:24:38 +0900

    added: Convert to enum

---
+++ src/CodeTips/Convert/ConvertToEnum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTips.Convert;
+
+internal class ConvertToEnum
+{
+    internal SomeStuffEnum Example()
+    {
+        var input = 1234;
+
+        if (Enum.IsDefined(typeof(SomeStuffEnum), input))
+        {
+            return (SomeStuffEnum)input; // int 값을 SomeEnum으로 변환
+        }
+        else
+        {
+            throw new ArgumentException("유효하지 않은 enum 값입니다.", nameof(input));
+        }
+    }
+}
+
+
+
+enum SomeStuffEnum
+{
+    FirstValue = 1234
+}
